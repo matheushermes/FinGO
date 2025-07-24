afb5754b960b3633819083f2ebbca58acf9395c2
Matheus Santos <matheus.hermes97@gmail.com>
2025-07-23 22:17:33 -0300

    criando conexão com o banco de dados

--- configs/config.go
+++ configs/config.go
@@ -1,6 +1,7 @@
 package configs
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -8,7 +9,13 @@ import (
 )
 
 var (
-	PORT = ""
+	PORT              = ""
+	POSTGRES_HOST      = ""
+	POSTGRES_USER      = ""
+	POSTGRES_PASSWORD  = ""
+	POSTGRES_DB        = ""
+	POSTGRES_PORT      = ""
+	STRING_CONNECTION  = ""
 )
 
 func LoadingEnv() {
@@ -20,4 +27,39 @@ func LoadingEnv() {
 	if PORT == "" {
 		log.Fatal("A variável de ambiente PORT não está definida no arquivo .env")
 	}
+
+	POSTGRES_HOST = os.Getenv("POSTGRES_HOST")
+	if POSTGRES_HOST == "" {
+		log.Fatal("A variável de ambiente POSTGRES_HOST não está definida no arquivo .env")
+	}
+
+	POSTGRES_USER = os.Getenv("POSTGRES_USER")
+	if POSTGRES_USER == "" {
+		log.Fatal("A variável de ambiente POSTGRES_USER não está definida no arquivo .env")
+	}
+	
+	POSTGRES_PASSWORD = os.Getenv("POSTGRES_PASSWORD")
+	if POSTGRES_PASSWORD == "" {
+		log.Fatal("A variável de ambiente POSTGRES_PASSWORD não está definida no arquivo .env")
+	}
+
+	POSTGRES_DB = os.Getenv("POSTGRES_DB")
+	if POSTGRES_DB == "" {
+		log.Fatal("A variável de ambiente POSTGRES_DB não está definida no arquivo .env")
+	}
+
+	POSTGRES_PORT = os.Getenv("POSTGRES_PORT")
+	if POSTGRES_PORT == "" {
+		log.Fatal("A variável de ambiente POSTGRES_PORT não está definida no arquivo .env")
+	}
+
+	STRING_CONNECTION = fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
 }
--- internal/database/db.go
+++ internal/database/db.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"database/sql"
+
+	"github.com/matheushermes/FinGO/configs"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	db, err := sql.Open("postgres", configs.STRING_CONNECTION)
+	if err != nil {
+		return nil, err
+	}
+	
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
