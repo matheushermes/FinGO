0f4702f5b60e11e96cb8a6bfaf4a8243bac860a0
Matheus Santos <matheus.hermes97@gmail.com>
2025-07-28 22:19:30 -0300

    implementa lógica de criação de usuários

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/matheushermes/FinGO/internal/database"
 	"github.com/matheushermes/FinGO/internal/models"
+	"github.com/matheushermes/FinGO/internal/repository"
 )
 
 func RegisterUser(c *gin.Context) {
@@ -15,5 +17,33 @@ func RegisterUser(c *gin.Context) {
 		return
 	}
 
-	
+	if err := user.IsValid("register"); err != nil {
+		c.JSON(404, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	db, err := database.ConnectDB()
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "failed to connect to the database",
+		})
+		return
+	}
+	defer db.Close()
+
+	repo := repository.NewUsersRepository(db)
+	userID, err := repo.Create(user)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "failed to create user in the database",
+		})
+		return
+	}
+
+	c.JSON(201, gin.H{
+		"message": "user registered successfully",
+		"userID":  userID,
+	})
 }
