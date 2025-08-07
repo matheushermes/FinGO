34d4fbbacb8452486142b54868d428807fcc14e2
Matheus Santos <matheus.hermes97@gmail.com>
2025-08-07 19:47:54 -0300

    feat(utils): adiciona utilitário para buscar preço de criptos no CoinGecko

---
+++ internal/utils/coingecko.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/matheushermes/FinGO/configs"
+)
+
+func GetPrinceFromCoinGecko(symbol string) (float64, error) {
+	symbol = strings.ToLower(symbol)
+
+	url := fmt.Sprintf("https://api.coingecko.com/api/v3/simple/price?symbols=%s&vs_currencies=usd", symbol)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return 0, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	// Insira sua API key aqui
+	req.Header.Set("x-cg-demo-api-key", configs.API_KEY_COINGECKO)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, fmt.Errorf("failed to fetch data from CoinGecko: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		return 0, fmt.Errorf("CoinGecko API error (%d): %s", resp.StatusCode, string(bodyBytes))
+	}
+
+	var result map[string]map[string]float64
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return 0, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	price, ok := result[symbol]["usd"]
+	if !ok {
+		return 0, fmt.Errorf("symbol %s not found in CoinGecko response", symbol)
+	}
+	
+	return price, nil
+}
