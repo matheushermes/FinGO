b696a40bafd983ad477c22cb4143fe773e1a17b2
Matheus Santos <matheus.hermes97@gmail.com>
2025-08-07 21:34:52 -0300

    feat(main): inicializa cliente Redis na aplicação

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	_ "github.com/matheushermes/FinGO/init"
+	"github.com/matheushermes/FinGO/internal/cache"
 	"github.com/matheushermes/FinGO/internal/server"
 )
 
 func main() {
 	fmt.Println("Iniciando projeto FinGO!")
+	cache.InitRedis()
 	server := server.NewServer()
 	server.RunServer()
 }
