6f523574a4146eb58e0041b52db879385dcf64ff
Matheus Santos <matheus.hermes97@gmail.com>
2025-08-26 22:22:34 -0300

    chore(main): iniciar worker de atualização de preços junto ao servidor

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	_ "github.com/matheushermes/FinGO/init"
 	"github.com/matheushermes/FinGO/internal/cache"
 	"github.com/matheushermes/FinGO/internal/server"
+	"github.com/matheushermes/FinGO/internal/worker"
 )
 
 func main() {
 	fmt.Println("Iniciando projeto FinGO!")
 	cache.InitRedis()
+	worker.StartCryptoUpdater(5 * time.Minute)
 	server := server.NewServer()
 	server.RunServer()
 }
