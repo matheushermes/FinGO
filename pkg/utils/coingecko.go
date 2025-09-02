91017c8a679a9def255f36bb7e43eb9ec0134343
Matheus Santos <matheus.hermes97@gmail.com>
2025-09-01 22:04:36 -0300

    feat(cache): implementa cache Redis para histórico de preços do CoinGecko

--- pkg/utils/coingecko.go
+++ pkg/utils/coingecko.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/matheushermes/FinGO/configs"
 	"github.com/matheushermes/FinGO/internal/cache"
+	"github.com/matheushermes/FinGO/internal/models"
 )
 
 func GetPriceFromCoinGecko(symbol string) (float64, error) {
@@ -57,4 +58,79 @@ func GetPriceFromCoinGecko(symbol string) (float64, error) {
 	cache.Set(cacheKey, fmt.Sprintf("%f", price), 15*time.Minute)
 	
 	return price, nil
+}
+
+func GetCryptoMarketChart(crypto, days, interval string) (*models.MarketChart, error) {
+	cacheKey := fmt.Sprintf("market_chart:%s:%s:%s", crypto, days, interval)
+
+	if cached, err := cache.Get(cacheKey); err != nil {
+		var result models.MarketChart
+		if err := json.Unmarshal([]byte(cached), &result); err == nil {
+			return &result, nil
+		}
+	}
+
+	url := fmt.Sprintf("https://api.coingecko.com/api/v3/coins/%s/market_chart?vs_currency=usd&days=%s&interval=%s", crypto, days, interval)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("x-cg-demo-api-key", configs.API_KEY_COINGECKO)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch data from CoinGecko: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("CoinGecko API error (%d): %s", resp.StatusCode, string(body))
+	}
+
+	var result models.MarketChart
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	cache.Set(cacheKey, string(body), 15*time.Minute)
+
+	return &result, nil
+}
+
+func GetCryptoMarketChartRange(crypto string, from, to time.Time) (*models.MarketChart, error) {
+	cacheKey := fmt.Sprintf("market_chart_range:%s:%d:%d", crypto, from.Unix(), to.Unix())
+
+	if cached, err := cache.Get(cacheKey); err == nil {
+		var result models.MarketChart
+		if err := json.Unmarshal([]byte(cached), &result); err == nil {
+			return &result, nil
+		}
+	}
+
+	url := fmt.Sprintf("https://api.coingecko.com/api/v3/coins/%s/market_chart/range?vs_currency=usd&from=%d&to=%d",
+	crypto, from.Unix(), to.Unix())
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("x-cg-demo-api-key", configs.API_KEY_COINGECKO)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("coingecko error %d: %s", resp.StatusCode, string(body))
+	}
+
+	var result models.MarketChart
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, err
+	}
+
+	cache.Set(cacheKey, string(body), 15*time.Minute)
+
+	return &result, nil
 }
